cc2860b502b09775c54e56637793f9e571339dae
unknown <ahmadfaishalhidayat.03@gmail.com>
2025-03-21 16:17:20 +0700

    setup routes provider onprogress

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { SignUpPage } from "./pages/SignUpPage";
+import { LoginPage } from "./pages/LoginPage";
+import { SettingPage } from "./pages/SettingPage";
+import { ProfilePage } from "./pages/ProfilePage";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+import { HomePage } from "./pages/HomePage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  // ... route definitions
+  createRoutesFromElements(
+    <Route path="/" element={<HomePage />}>
+      <Route path="/signin" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/setting" element={<SettingPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Route>
+  )
+);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
---
+++ FrontEnd/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-10">Home Page</h1>
+    </div>
+  );
+};
